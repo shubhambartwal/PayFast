1f8517d88833f27bb884d66c139ce02d65503e05
shubhambartwal <shubhambartwal58@gmail.com>
2024-09-05 23:27:26 +0530

    typescript configurations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express'
-import rootRouter from './routes'
+import rootRouter from './routes/index'
 import cors from 'cors'
 const app= express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1',rootRouter)
-app.listen(3000)
+app.listen(3000,()=>console.log("server is running"))
